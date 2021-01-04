e508044cca35a769ed7712278d06f8a6946eac27
Alexander Nikolaychuk <alexksnikol@gmail.com>
2021-01-04 14:38:01 +0200

    Extract `FeedStore` to it's own file on production

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Alexander Nikolaychuk on 04.01.2021.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
