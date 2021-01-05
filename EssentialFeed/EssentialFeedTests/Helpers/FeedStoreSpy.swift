3f927c5b558143621db8736fedb6cef2b71b6092
Alexander Nikolaychuk <alexksnikol@gmail.com>
2021-01-05 11:15:08 +0200

    Load command cache retrieval

--- EssentialFeed/EssentialFeedTests/Helpers/FeedStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/FeedStoreSpy.swift
@@ -13,6 +13,7 @@ class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private var deletionCompletion = [DeletionCompletion]()
@@ -43,6 +44,10 @@ class FeedStoreSpy: FeedStore {
     
     func completeInsertionSuccessfully(at index: Int = 0) {
         insertionCompletions[index](nil)
+    }
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
     }
     
 }
