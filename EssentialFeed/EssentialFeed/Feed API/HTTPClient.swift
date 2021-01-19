c2f9ee4098927cb12e675877c071fed089fff404
Alexander Nikolaychuk <alexksnikol@gmail.com>
2021-01-19 17:27:00 +0200

    /// The completion handler can be invoked in any thread.     /// Clients are responsible to dispatch to approriate threads, in needed.

--- EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 public protocol HTTPClient {
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to approriate threads, in needed.
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
